827283ffeda3c83a0620af8d2b898fd9e73cb81d
Sebastian Rojo <arpagon@gmail.com>
2013-10-03 17:48:29 -0400

    Primera Revision Tarea 2

--- assignament2/pyloc.py
+++ assignament2/pyloc.py
@@ -56,24 +56,24 @@ class Item(object):
         Init
         '''
         self.line=line
-        self.position=start_line
+        self.start_line=start_line
+        self.end_line=False
         self.name=False
         self.ident_level=False
+        self.length=0
         self.sub_items={}
-        if not self.IdentifyPart():
-            del self
+        self.is_a_part=self.IdentifyPart()
         
-    def IdentifyPart(self.line):
+    def IdentifyPart(self):
         '''
         Extract Item o Part Name. from line
         '''
-        idents=line.split("    ")
-        ident_level=0
+        idents=self.line.rstrip().split("    ")
+        self.ident_level=len(idents) - 1
         for level in idents:
             if level.startswith("def ") or level.startswith("class "):
                 self.name=level.split(" ")[1].split("(")[0]
-                self.ident_level=ident_level
-            ident_level+=1
+                return True
         return False
 
 
@@ -98,29 +98,71 @@ def LOCCount(file):
     blank_lines=0
     comment_lines=0
     parts = {}
-    
+    in_part=False
+    running_part=False
+            
     with open(file, 'rb') as code_file:
         for line in code_file:
-            in_part=False
-            running_part=False
+            log.debug("RUNINGPART:%s  INPART:%s" % (running_part, in_part))
             total_lines+=1
-            if line.startswith("#"):
+            if line.strip().startswith("#"):
                 comment_lines+=1
-            elif line.strip():
+                log.debug("COMENT LINE %s %s" % (comment_lines, line))
+            elif not line.strip():
                 blank_lines+=1
+                log.debug("BLANK LINE %s %s" % (blank_lines, line))
             else:
                 code_lines+=1
-                runining_item=Item(line)
-                if runining_item.ident_level=0:
-                    '''Is a Part'''
-                    parts[runining_item.name]=runining_item
-                    running_part=runining_item.name
-                    
-                    
-                    
-                            
-                    
+                log.debug("CODE LINE %s %s" % (code_lines, line))
+                running_item=Item(line, total_lines)
+                if running_item.is_a_part:
+                    if running_item.ident_level==0:
+                        '''Is a Part'''
+                        parts[running_item.name]=running_item
+                        running_part=running_item.name
+                        parts[running_part].length+=1
+                        in_part=True
+                        logpart=logging.getLogger('LOC.%s' % running_part)
+                        log.debug("ADDED PART %s %s" % (running_item.name, 
+                                                                        line))
+                    elif running_item.ident_level==1:
+                        #logpart.debug("runinng_part %s: running_item.name : %s" % (running_part, running_item.name))
+                        parts[running_part].sub_items[running_item.name]=running_item
+                        parts[running_part].length+=1
+                        logpart.debug("IN PART %s  ADD ITEM %s: %s" % (running_part, 
+                                                    running_item.name, line))
+                    else:
+                        parts[running_part].length+=1
+                        logpart.debug("PART LENGTH %s: %s" % (running_part, 
+                                            parts[running_part].length, line))
+                else:
+                    if in_part:
+                        logpart.debug("IDENT: %s" % running_item.ident_level)
+                        if running_item.ident_level>0:
+                            parts[running_part].length+=1
+                            logpart.debug("PART LENGTH %s: %s" % (running_part, 
+                                            parts[running_part].length))
+                        else:
+                            parts[running_part].end_line=total_lines
+                            in_part=False
+                            del running_item
+    log.debug("===TOTAL===\n CODE LINES:%s\n PARTS:%s \n TOTAL LINES:%s \n "
+              "BLANK LINE:%s \n COMMEN_LINES:%s" % 
+              (code_lines, parts, total_lines, blank_lines, comment_lines))
+    return (code_lines, parts, total_lines, blank_lines, comment_lines)
+
+def FormatOutput(code_lines, parts, total_lines=0, blank_lines=0, comment_lines=0):
+    '''
+    OutPut whit the optimal Format.
+    '''
+    print "===Parts==="
+    for part in parts:
+        
+        print (parts[part].name, parts[part].length)
+        print parts[part].sub_items.keys()
     
+    print "===Totals==="
+    print code_lines
 
 
 def main():
@@ -132,9 +174,10 @@ def main():
     (options, args) = parser.parse_args()
     log.info("START COUNTING")
     if options.file:
-        result=LOCCount(options.file)
+        (code_lines, parts, total_lines, blank_lines, comment_lines)=LOCCount(options.file)
+        FormatOutput(code_lines, parts, total_lines, blank_lines, comment_lines)
     else:
-        parser.error("please define File,  %prog -F example.csv\n Please use -h for help")
+        parser.error("please define File,  %prog -F example.py\n Please use -h for help")
 
 if __name__=='__main__':
     main()
